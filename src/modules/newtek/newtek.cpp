09f5f1cea388d88f9deace76191283c9de1663f9
ianshade <zegzulakrzysztof@gmail.com>
2018-11-17 13:10:47 +0100

    Unified consumer and producer naming (newtek-ndi -> ndi)

--- src/modules/newtek/newtek.cpp
+++ src/modules/newtek/newtek.cpp
@@ -44,7 +44,7 @@ void init(core::module_dependencies dependencies)
                                                                                 create_preconfigured_ivga_consumer);
 
         dependencies.consumer_registry->register_consumer_factory(L"NDI Consumer", create_ndi_consumer);
-        dependencies.consumer_registry->register_preconfigured_consumer_factory(L"newtek-ndi",
+        dependencies.consumer_registry->register_preconfigured_consumer_factory(L"ndi",
                                                                                 create_preconfigured_ndi_consumer);
 
         dependencies.producer_registry->register_producer_factory(L"NDI Producer", create_ndi_producer);
